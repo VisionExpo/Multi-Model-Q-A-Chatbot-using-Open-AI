7fec51cdf95e148f564054c9091008a7e7c49053
Vishal Gorule <gorulevishal984@gmail.com>
2025-07-24 22:10:33 +0530

    Add initial app and requirements files for Multi-Model Q&A Chatbot

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+from os
+from dotenv import load_dotenv
+
+load_dotenv()
+
